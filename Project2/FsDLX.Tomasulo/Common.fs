e849dfc377bc9b068af8ac52c5c7e45b923342e1
Aaron Brewbaker <brewbaker.aaron@gmail.com>
2014-12-10 15:54:02 -0500

    functional unit config

---
+++ Project2/FsDLX.Tomasulo/Common.fs
@@ -0,0 +1,31 @@
+﻿[<AutoOpen>]
+module FsDLX.Tomasulo.Common
+
+type FunctionalUnitConfig =
+    {
+        nReservationStations : int
+        nExecutionUnits : int
+        nExecutionTime : int
+        Instructions : string[]
+    }
+
+    static member IntegerUnit =
+        { nReservationStations = 8; nExecutionUnits = 3; nExecutionTime = 1;
+            Instructions = [| "addi"; "nop"; "add"; "sub"; "and"; "or"; "xor"; "movf"; "movfp2i"; "movi2fp" |]}
+
+    static member TrapUnit =
+        { nReservationStations = 4; nExecutionUnits = 1; nExecutionTime = 1;
+            Instructions = [| "trap" |]}
+
+    static member BranchUnit =
+        { nReservationStations = 1; nExecutionUnits = 1; nExecutionTime = 1;
+            Instructions = [| "beqqz"; "j"; "jr"; "jal"; "jalr" |]}
+
+    static member MemoryUnit =
+        { nReservationStations = 8; nExecutionUnits = 1; nExecutionTime = 2;
+            Instructions = [| "lw"; "lf"; "sw"; "sf" |]}
+
+    static member FloatingPointUnit =
+        { nReservationStations = 8; nExecutionUnits = 2; nExecutionTime = 4;
+            Instructions = [| "addf"; "subf"; "multf"; "divf"; "mult"; "div"; "cvtf2i"; "cvti2f" |]}
+
